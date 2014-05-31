03526ddf71bcab57aee064dbad5ffa10beb4772c
axkhan2 <arif788@gmail.com>
2014-05-31 07:44:33 +0000

    Committing peoplement projects

---
+++ Java/CoreJava/GitTest/src/com/test/TestMain.java
@@ -0,0 +1,14 @@
+package com.test;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+       TestPrint tp = new TestPrint();
+       tp.PublicDisplay("Hello Public");
+       tp.DefulatDisplay("Hello Default");
+	}
+
+}
---
+++ Java/CoreJava/GitTest/src/com/test/TestPrint.java
@@ -0,0 +1,12 @@
+package com.test;
+
+class TestPrint {
+	public void PublicDisplay(String str) {
+		System.out.println("Public " + str);
+	}
+
+	public void DefulatDisplay(String str) {
+		System.out.println("Default " + str);
+	}
+
+}
